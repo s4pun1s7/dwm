f45a493dd9257b45d7fa67bc527d0962998df21f
mk <m.k4lchev@proton.me>
2025-08-26 01:31:33 +0300

    update & patches

--- config.def.h
+++ config.def.h
@@ -4,16 +4,21 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int barpadv	= 10;		/* bar vertical padding (from top)*/
+static const unsigned int barpadh	= 10;		/* bar vertical padding (from top)*/
+static const unsigned int barheight	= 2;		/* bar vertical padding (from top)*/
+static const unsigned int barborder	= 2;		/* bar vertical padding (from top)*/
+static const unsigned int floatbar	= 1;		/* 0 means bar won't float; float or dock the bar */
+static const unsigned int snap      = 16;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_gray1[]       = "#002b36";
+static const char col_gray2[]       = "#073642";
+static const char col_gray3[]       = "#839496";
+static const char col_gray4[]       = "#93a1a1";
+static const char col_cyan[]        = "#174956";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -25,20 +30,30 @@ static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 static const char *tagsalt[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 static const int momentaryalttags = 0; /* 1 means alttags will show only when key is held down*/
 
+/* appicons */
+/* NOTE: set to 0 to set to default (whitespace) */
+static char outer_separator_beg      = '[';
+static char outer_separator_end      = ']';
+static char inner_separator          = ' ';
+static unsigned truncate_icons_after = 2; /* will default to 1, that is the min */
+static char truncate_symbol[]         = "...";
+
 static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class        instance    title    tags mask     isfloating   monitor */
+	/* class        instance    title    tags mask     isfloating   monitor		appicon*/
 	{ "Gimp",       NULL,       NULL,       0,            1,           -1 },
-	{ "st-256color", NULL,       NULL,       0,            1,           -1 },
-	{ "Emulator",    NULL,       NULL,       0,            1,           -1 },
-	{ "Google-chrome", NULL,     NULL,       0,            1,           -1 },
+	{ "inlyne",     NULL,       NULL,       0,            1,           -1 },
+	{ "st",         NULL,       NULL,       0,            0,           -1 },
+	{ "st-256color", NULL,       NULL,       0,            1,          -1 },
+	{ "Emulator",    NULL,       NULL,       0,            1,          -1 },
+	{ "Google-chrome", NULL,     NULL,       0,            1,          -1 },
 	{ "Guake",      NULL,       NULL,       0,            1,           -1 },
-	{ "ViberPC",  NULL,       NULL,        0,            1,           -1 },
-	{ "TelegramDesktop", NULL,   NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "ViberPC",  NULL,       NULL,        0,            1,            -1 },
+	{ "TelegramDesktop", NULL,   NULL,       0,            1,          -1 },
+	{ "Firefox",  NULL,       NULL,       1 << 8,       0,             -1 },
 };
 
 /* layout(s) */
@@ -69,19 +84,22 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
+static const char *tgcmd[]  = { "telegram-desktop", NULL };
 static const char *htopcmd[]  = { "st", "-e", "htop", NULL };
+static const char *fmcmd[]  = { "st", "-e", "ranger", NULL };
 static const char *incvol[]  = { "/ust/bin/amixer", "set", "Master", "5+", NULL };
 static const char *decvol[]  = { "/ust/bin/amixer", "set", "Master", "5-", NULL };
-/*static const char *upbrightness[]   = { "xbacklight", "-inc", "10", NULL };
-static const char *downbrightness[] = { "xbacklight", "-dec", "10", NULL }; */
+static const char *upbrightness[]   = { "brightnessctl", "set", "+8", NULL };
+static const char *downbrightness[] = { "brightnessctl", "set", "8-", NULL }; 
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                 		XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                 		XK_r,       spawn,          {.v = htopcmd } },
+	{ MODKEY,                 		XK_r,      spawn,          {.v = htopcmd } },
+	{ MODKEY,                 		XK_e,      spawn,          {.v = fmcmd } },
+	{ MODKEY,                 		XK_t,      spawn,          {.v = tgcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_n,      togglealttag,   {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
@@ -94,18 +112,20 @@ static Key keys[] = {
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_r,      setlayout,      {.v = &layouts[3]} },
+	{ MODKEY|ShiftMask,             XK_r,      setlayout,      {.v = &layouts[4]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
 	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
 	{ 0,            XF86XK_AudioLowerVolume,   spawn,          {.v = decvol } },
 	{ 0,            XF86XK_AudioRaiseVolume,   spawn,          {.v = incvol } },
-/*
 	{ 0,            XF86XK_MonBrightnessUp,    spawn,          {.v = upbrightness } },
 	{ 0,            XF86XK_MonBrightnessDown,  spawn,          {.v = downbrightness } },
-*/
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
