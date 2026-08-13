da881ac933f464093d54aeabd386a6b01a66cede
Martin Kalchev <19384056+s4pun1s7@users.noreply.github.com>
2026-08-13 19:49:16 +0300

    Enable the autostart patch
    dwm now runs ~/.local/share/dwm/autostart.sh itself, so start-dwm no
    longer has to launch it from ~/.config/autostart.

--- patches.def.h
+++ patches.def.h
@@ -509,7 +509,7 @@
  * than the original ~/.dwm folder.
  * https://dwm.suckless.org/patches/autostart/
  */
-#define AUTOSTART_PATCH 0
+#define AUTOSTART_PATCH 1
 
 /* By default, windows that are not visible when requesting a resize/move will not
  * get resized/moved. With this patch, they will.
